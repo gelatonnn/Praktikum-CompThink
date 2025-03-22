3341ef48fb7de092736700a323bdd5978ad2916b
Galton <francisgaltonnn@gmail.com>
2025-03-22 14:15:54 +0700

    Update and rename CT Soal 1 Ganjil Genap.py to GanjilGenap.py

---
+++ Praktikum 1/Latihan/GanjilGenap.py
@@ -0,0 +1,5 @@
+nilai = int(input ("Masukan angka : "))
+if (nilai %2 == 0):
+    print(str(nilai) + " adalah bilangan genap")
+elif (nilai %2 == 1):
+    print(str(nilai) + " adalah bilangan ganjil")
