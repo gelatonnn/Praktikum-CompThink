0a62f9cdd339762144c182cf51474f28ec5321a8
Galton <francisgaltonnn@gmail.com>
2025-03-22 14:22:33 +0700

    Delete Latihan directory

--- Latihan/Fungsi.py
+++
@@ -1,7 +0,0 @@
-def kuadrat(x):
-    x2 = x * x
-    return x2
-
-n = int(input("Masukan Nilai : "))
-n2 = kuadrat(n)
-print(n2)
--- Latihan/Latihan Fungsi Kuadrat.py
+++
@@ -1,9 +0,0 @@
-def fungsi(x):
-    x = x ** 2 - 2*x + 5
-    return x 
-
-a = int(input("Masukan A : "))
-b = int(input("Masukan B : "))
-
-for i in range(a, b+1):
-    print(f'f({i}) = {fungsi(i)}')
