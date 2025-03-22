2ad1881a86f32c9cd3040e3bdef35db98bcc9134
Galton <francisgaltonnn@gmail.com>
2025-03-22 14:18:56 +0700

    Update Palindorm.py

--- Praktikum 2/Latihan/Palindorm.py
+++ Praktikum 2/Latihan/Palindorm.py
@@ -1,1 +1,10 @@
-
+n = int(input("Masukkan panjang string: "))
+string = str(input("Masukkan string: "))
+palindrom = True
+for i in range(n//2):
+    if string[i] != string[n-1-i]:
+        palindrom = False
+if palindrom:
+    print(f"{string} adalah palindrom")
+else:
+    print(f"{string} bukan palindrom")
